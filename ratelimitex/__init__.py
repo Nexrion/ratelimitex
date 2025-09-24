d4266e7dc5fc04c6a85f15818a346f8d306a7a02
Dor Bar <dor.b4r@gmail.com>
2025-09-24 12:28:16 +0300

    Setup ratelimitex as a PyPi package

--- ratelimitex/__init__.py
+++ ratelimitex/__init__.py
@@ -2,11 +2,18 @@
 Rate Limiter module for managing API rate limits with a simple DX.
 """
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .core import RateLimiter
 from .models import RateLimitConfig, RateLimitStrategy, RateLimiterStats
 from .client import RateLimitedClient, configure
 from .decorators import rate_limited, adaptive_rate_limited, burst_rate_limited
 from .utils import is_rate_limit_error
+
+try:
+    __version__ = version("ratelimitex")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 __all__ = [
     'RateLimitedClient',
